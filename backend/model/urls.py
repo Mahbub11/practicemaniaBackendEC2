b07e190c12bde8ab030679d27f13e649675f0acd
Mahbub Rahim <mahbubrahim926@gmail.com>
2023-11-27 00:49:12 +0600

    Update urls.py

--- backend/model/urls.py
+++ backend/model/urls.py
@@ -3,7 +3,7 @@ from . import views
 from rest_framework.authtoken.views import obtain_auth_token 
 
 urlpatterns =[
-    path('checkgrammar',views.checkSentenceGrammar),
+    # path('checkgrammar',views.checkSentenceGrammar),
     
     
 ]
